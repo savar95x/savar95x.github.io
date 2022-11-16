13c072ca3e1520126ba612d46eca701d38733565
savar <skaar513@gmail.com>
2022-11-16 15:22:59 +0530

    removed unused functions

--- js/editMenu.js
+++ js/editMenu.js
@@ -1,21 +1,9 @@
 toggleEngine();
 
-function editBookmarks() {
-	document.getElementById("editContainer").style.display = "initial";
-	document.getElementById("mainContainer").style.opacity = "0.25";
-	document.getElementById("mainContainer").style["pointer-events"] = "none";
-}
-
-function backToHome() {
-	document.getElementById("editContainer").style.display = "none";
-	document.getElementById("mainContainer").style.opacity = "1";
-	document.getElementById("mainContainer").style["pointer-events"] = "auto";
-}
-
 function sourceCode() {
 	window.open("https://github.com/skaar513/skaar513.github.io", "_self")
 }
 
 function openWebsite() {
-	window.open("old/movies.html", "_self")
+	window.open("misc/old/movies.html", "_self")
 }
