4e55d512d2230d5877cc14a2efa5f91b2f2fef1e
savar <skaar513@gmail.com>
2022-11-18 01:44:40 +0530

    updated descriptions

--- misc/old/js/homepage.js
+++ misc/old/js/homepage.js
@@ -1,6 +1,6 @@
 const allEngines = [
- { name:"SearX", brief:"A Decentralized MetaSearch Engine", color:"#8AE29D", url:"searx.be/search?q=" },
- { name:"Brave", brief:"The Search Engine From Brave Software", color:"#FC8A84", url:"search.brave.com/search?q=" },
- { name:"ArchWiki", brief:"The Biggest Wiki For Linux Users", color:"#A8BEFB", url:"wiki.archlinux.org/index.php?search=" },
- { name:"YouTube", brief:"A video-sharing/social-media platform", color:"#FC8A84", url:"youtube.com/results?search_query=" }
+ { name:"SearX", brief:"a decentralized metasearch engine", color:"#8AE29D", url:"searx.be/search?q=" },
+ { name:"Brave", brief:"search engine from brave software", color:"#FC8A84", url:"search.brave.com/search?q=" },
+ { name:"ArchWiki", brief:"a wiki For linux users", color:"#A8BEFB", url:"wiki.archlinux.org/index.php?search=" },
+ { name:"YouTube", brief:"a video-sharing/social-media platform", color:"#FC8A84", url:"youtube.com/results?search_query=" }
 ];
