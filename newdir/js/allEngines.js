1b0e4bb57934d5ce4d3069a57607aefedc05454c
savar <skaar513@gmail.com>
2023-01-14 12:59:43 +0530

    added searx and youtube as engines

--- newdir/js/allEngines.js
+++ newdir/js/allEngines.js
@@ -1,7 +1,7 @@
 const allEngines = [
- //{ name:"SearX", preposition:"with", color:"#8aac8b", url:"searx.be/search?q=", urlFilters:"" },
+ { name:"SearX", preposition:"with", color:"#8aac8b", url:"searx.be/search?q=", urlFilters:"" },
  { name:"Google", preposition:"with", color:"hsl(355,75%,76%)", url:"google.com/search?q=", urlFilters:"" },
  { name:"ArchWiki", preposition:"over", color:"#9691b3", url:"wiki.archlinux.org/index.php?search=", urlFilters:"" },
- //{ name:"YouTube", preposition:"over", color:"#ac8a8c", url:"youtube.com/results?search_query=", urlFilters:"" },
+ { name:"YouTube", preposition:"over", color:"#ac8a8c", url:"youtube.com/results?search_query=", urlFilters:"" },
  { name:"Github", preposition:"over", color:"#8fb4b5", url:"github.com/search?q=", urlFilters:"&s=stars" },
 ];
