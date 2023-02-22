b007d0209fcfc16336510ee0b12c2ee536229692
savar <skaar513@gmail.com>
2023-02-22 17:40:40 +0530

    added wikipedia

--- newdir/js/allEngines.js
+++ newdir/js/allEngines.js
@@ -1,6 +1,7 @@
 const allEngines = [
  { name:"Google", preposition:"with", color:"rgba(143, 138, 172, 0.85)", url:"google.com/search?q=", urlFilters:"", image:"google.png" },
  //{ name:"SearX", preposition:"with", color:"rgba(138, 172, 139, 0.85)", url:"searx.be/search?q=", urlFilters:"", image:"searx.png" },
+ { name:"Wikipedia", preposition:"over", color:"#ACA98A", url:"wikipedia.org/w/index.php?fulltext=1&search=", urlFilters:"", image:"wikipedia.jpg" },
  { name:"GentooWiki", preposition:"over", color:"rgba(181, 143, 181, 0.85)", url:"wiki.gentoo.org/index.php?title=Special%3ASearch&search=", urlFilters:"&go=Go", image:"gentoo.jpg" },
  //{ name:"YouTube", preposition:"over", color:"rgba(172, 138, 140, 0.85)", url:"youtube.com/results?search_query=", urlFilters:"", image:"youtube.png" },
  { name:"Github", preposition:"over", color:"#535353", url:"github.com/search?q=", urlFilters:"&s=stars", image:"github.png" },
