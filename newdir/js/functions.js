8e8b6b31b1bd5a5c8a5b059c8f7d2dbbe08621d4
savar <skaar513@gmail.com>
2023-02-27 22:38:21 +0530

    added back clearbtn

--- newdir/js/functions.js
+++ newdir/js/functions.js
@@ -1,30 +1,31 @@
-	var activeEngine;
-	var x = -1;
+var activeEngine;
+var x = -1;
 
-	function toggleEngine() { x = (x == allEngines.length - 1) ? -1:x; 
-		x = x + 1;
-		activeEngine = allEngines[x];
-		document.getElementById('searchInput').placeholder = "Search " + activeEngine.preposition + " " + activeEngine.name;
-//		document.getElementById('myicon').src = "imgs/" + activeEngine.image;
-//		document.getElementById('searchInput').style.color = activeEngine.color;
-//		document.getElementById('searchContainer').style.boxShadow = "0px 1px 1px " + activeEngine.color;
-		document.getElementById('toggleButton').style.color = activeEngine.color;
-		document.getElementById('searchInput').focus(); };
+function toggleEngine() {
+  x = (x == allEngines.length - 1) ? -1:x; 
+	x = x + 1;
+	activeEngine = allEngines[x];
+	document.getElementById('searchInput').placeholder = "Search " + activeEngine.preposition + " " + activeEngine.name;
+	document.getElementById('toggleButton').style.color = activeEngine.color;
+	document.getElementById('searchInput').focus();
+};
 
-	function search() {
-    let realVal = document.getElementById('searchInput').value.replace("%","%25").replace(/#/g,"%23").replace(/&/g,"%26").replaceAll("+","%2B").replace("/","%2F").replace("?","%3F").replace("=","%3d").replace("_","%5F").replace(/:/g,"%3A");
-		let noSpaceVal = realVal.replace(/\s/g, "");
+function search() {
+  let realVal = document.getElementById('searchInput').value.replace("%","%25").replace(/#/g,"%23").replace(/&/g,"%26").replaceAll("+","%2B").replace("/","%2F").replace("?","%3F").replace("=","%3d").replace("_","%5F").replace(/:/g,"%3A");
 
-		if (noSpaceVal == "") window.location.reload(false);
-		else window.open("https://" + activeEngine.url + realVal + activeEngine.urlFilters, "_self");
-	};
+	if (realVal.replace(/\s/g, "") == "") window.location.reload(false);
+  else if (document.getElementById('searchInput').value.slice(-3) == " ul") window.open("https://" + document.getElementById('searchInput').value.slice(0,-3), "_self");
+	else window.open("https://" + activeEngine.url + realVal + activeEngine.urlFilters, "_self");
+};
 
-	function clearInput() {
-    document.getElementById("searchInput").value = "";
-		document.getElementById('searchInput').focus();
-		hideClearButton();
-  };
+function clearInput() {
+  document.getElementById("searchInput").value = "";
+	document.getElementById('searchInput').focus();
+	hideClearButton();
+};
 
-	function hideClearButton() {
-			document.getElementById("clearButton").style.display = (document.getElementById("searchInput").value == "") ? "none":"inline";
-	};
+function hideClearButton() {
+	document.getElementById("clearButton").style.display = (document.getElementById("searchInput").value == "") ? "none":"inline";
+};
+
+toggleEngine();
