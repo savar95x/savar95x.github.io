6ce681286acf4d92a111135f1bdefb03ecd8962e
savar <savar@oasis.localdomain>
2023-07-04 10:08:56 +0530

    gruv

---
+++ home/5/js/allEngines.js
@@ -0,0 +1,8 @@
+const allEngines = [
+ { name:"DuckDuckGo", preposition:"with", color:"hsl(355,75%,76%)", url:"duckduckgo.com/?q=", urlFilters:"" },
+ //{ name:"Google", preposition:"with", color:"hsl(355,75%,76%)", url:"google.com/search?q=", urlFilters:"" },
+ //{ name:"SearX", preposition:"with", color:"#8aac8b", url:"searx.be/search?q=", urlFilters:"" },
+ { name:"ArchWiki", preposition:"over", color:"#9691b3", url:"wiki.archlinux.org/index.php?search=", urlFilters:"" },
+ { name:"YouTube", preposition:"over", color:"#ac8a8c", url:"youtube.com/results?search_query=", urlFilters:"" },
+ { name:"Github", preposition:"over", color:"#8fb4b5", url:"github.com/search?q=", urlFilters:"&s=stars" },
+];
