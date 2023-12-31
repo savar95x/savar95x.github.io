317db6eeb10e88c918fb52d03ba0a29ab25f1dd0
savar95x <skaar513@gmail.com>
2023-12-31 20:42:43 +0530

    new gruv page

---
+++ home/6/js/allEngines.js
@@ -0,0 +1,7 @@
+const allEngines = [
+ { name:"SearX", preposition:"with", color:"#95AB63", url:"searx.be/search?q=", urlFilters:"" },
+ //{ name:"Google", preposition:"with", color:"hsl(355,75%,76%)", url:"google.com/search?q=", urlFilters:"" },
+ { name:"ArchWiki", preposition:"over", color:"#8DAFC1", url:"wiki.archlinux.org/index.php?search=", urlFilters:"" },
+ { name:"YouTube", preposition:"over", color:"#D87873", url:"youtube.com/results?search_query=", urlFilters:"" },
+ { name:"Github", preposition:"over", color:"#d487c9", url:"github.com/search?q=", urlFilters:"&s=stars" },
+];
