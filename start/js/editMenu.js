22475ade705ad3a92479207353e7bb3581526f0e
savar95x <skaar513@gmail.com>
2024-01-08 11:09:35 +0530

    js for homepage

---
+++ start/js/editMenu.js
@@ -0,0 +1,11 @@
+toggleEngine();
+
+function sourceCode() {
+	window.open("https://github.com/skaar513/skaar513.github.io", "_self")
+}
+
+function openWebsite() {
+	//window.open("misc/old/movies.html", "_self")
+	//window.open("https://keep.google.com", "_self")
+	window.open("https://savar95x.github.io", "_self")
+}
