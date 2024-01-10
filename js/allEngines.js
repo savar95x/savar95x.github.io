fd178d092eadd22d6523bd1c5de76567fb2e4ddc
savar95x <skaar513@gmail.com>
2024-01-10 21:59:23 +0530

    some organising

--- js/allEngines.js
+++
@@ -1,7 +0,0 @@
-const allEngines = [
- { name:"SearX", preposition:"with", color:"#A9B665", url:"searx.be/search?q=", urlFilters:"" },
- //{ name:"Google", preposition:"with", color:"hsl(355,75%,76%)", url:"google.com/search?q=", urlFilters:"" },
- { name:"ArchWiki", preposition:"over", color:"#80A9A0", url:"wiki.archlinux.org/index.php?search=", urlFilters:"" },
- { name:"YouTube", preposition:"over", color:"#D87873", url:"youtube.com/results?search_query=", urlFilters:"" },
- { name:"Github", preposition:"over", color:"#D8A657", url:"github.com/search?q=", urlFilters:"&s=stars" },
-];
