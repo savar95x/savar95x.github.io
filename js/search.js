cf3e1be9609041e0705011530a87fc4dc57889a4
savar <skaar513@gmail.com>
2025-12-07 00:12:45 +0530

    searchpage better

--- js/search.js
+++ js/search.js
@@ -6,15 +6,38 @@ const allEngines = [
 	{ name:"Github", logo:"ph-fill ph-github-logo", preposition:"over", color:"#D8A657", url:"github.com/search?q=", urlFilters:"&s=stars" },
 ];
 
+const searchInput = document.getElementById("searchInput");
+
 var activeEngine;
 var x = -1;
 
-function toggleEngine() {
-	x = (x == allEngines.length - 1) ? -1:x; 
-	x = x + 1;
+function toggleActiveColor(x) {
+	document.getElementById("engine-" + x).className = "engine active-engine";
+	for (let i = 1; i < allEngines.length; i++) {
+	document.getElementById("engine-" + ((x+i) % len)).className = "engine";
+	}
 	activeEngine = allEngines[x];
+
 	document.getElementById('searchInput').placeholder = "Search " + activeEngine.preposition + " " + activeEngine.name + " ...";
-	document.getElementById('search-icon').className = activeEngine.logo;
+	document.getElementById('searchInput').focus();
+}
+
+function toggleEngine() {
+	len = allEngines.length;
+	x++;
+	x = x%len;
+	activeEngine = allEngines[x];
+
+	//document.getElementById("engine-" + x).style.background = "#61A6FA";
+	//bg = document.getElementById("wrapper").style.background;
+	//document.getElementById("engine-" + x).style.color = "#000000";
+	//x = (x == 0) ? len : x;
+	//document.getElementById("engine-" + ((x-1) % len)).style.background = "#1a1a1a";
+	//document.getElementById("engine-" + ((x-1) % len)).style.color = "#808080";
+	toggleActiveColor(x);
+
+	document.getElementById('searchInput').placeholder = "Search " + activeEngine.preposition + " " + activeEngine.name + " ...";
+	//document.getElementById('search-icon').className = activeEngine.logo;
 	//document.getElementById('search-icon').style.borderColor = activeEngine.color;
 	document.getElementById('searchInput').focus();
 }
@@ -36,6 +59,13 @@ function search() {
 
 toggleClearButton();
 searchInput.addEventListener("input", toggleClearButton);
+const childBtn = document.getElementById("tabButton");
+
+// 1. Prevent the button click from stealing focus
+childBtn.addEventListener("mousedown", (e) => {
+    e.preventDefault(); 
+    // This stops the input from blurring, so the "hide" logic never runs!
+});
 
 function clearInput() {
 	document.getElementById("searchInput").value = "";
