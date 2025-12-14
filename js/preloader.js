56a366cd29df780c050794ba3d367676821901bb
savar <skaar513@gmail.com>
2025-12-15 02:19:26 +0530

    preload again

--- js/preloader.js
+++ js/preloader.js
@@ -1,15 +1,13 @@
-//Promise.all([
-//        // Wait for the HTML structure and CSS to parse
-//        new Promise(resolve => {
-//            if (document.readyState === 'loading') {
-//                document.addEventListener('DOMContentLoaded', resolve);
-//            } else {
-//                resolve(); // DOM was already ready
-//            }
-//        }),
-//        // Wait for the fonts to actually load
-//        document.fonts.ready 
-//    ]).then(() => {
-//        // Both are done. Reveal the page.
-//        document.documentElement.classList.add('wf-active');
-//    });
+    Promise.all([
+        new Promise(resolve => {
+            if (document.readyState === 'loading') {
+                document.addEventListener('DOMContentLoaded', resolve);
+            } else {
+                resolve();
+            }
+        }),
+        document.fonts.ready
+    ]).then(() => {
+        // Change this line to target the BODY
+        document.body.classList.add('wf-active');
+    });
